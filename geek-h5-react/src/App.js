202100cc2c4abde8962084215d791c3bc5613c4d
superquan <liquanai996@qq.com>
2022-05-24 23:26:18 +0800

    初始化

---
+++ geek-h5-react/src/App.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: quan
+ * @Date: 2022-05-24 22:35:44
+ * @LastEditors: quan
+ * @LastEditTime: 2022-05-24 23:12:32
+ * @Description: file content
+ */
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  )
+}
---
+++ geek-h5-react/src/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: quan
+ * @Date: 2022-05-24 22:35:44
+ * @LastEditors: quan
+ * @LastEditTime: 2022-05-24 23:12:08
+ * @Description: file content
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
